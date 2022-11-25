2c5046a75f2f94f30b426cf7d31740cf725eb063
Maneru31 <daniil.makarov03@mail.ru>
2022-11-25 19:38:52 +0300

    Git1

---
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.git.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.
+
+
+    }
+}
